5ceb1f06a393612731805dfa29993e0d8e8a445c
jesty1313 <107418805+jesty1313@users.noreply.github.com>
2022-06-13 14:54:11 -0400

    Update streamlit_app.py
    Added Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Heathly Diner')
+
+Breakfast Menu
+
+Omega 3 & Blueberrry Oatmeal
+
+Kale, Spinach & Rocket Smoothie
+
+Hard-Boiled Free Range Egg

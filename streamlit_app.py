aa1517ba43edf3cad52f5c6ac0e24ce5cf2be7f7
jesty1313 <107418805+jesty1313@users.noreply.github.com>
2022-06-14 08:58:33 -0400

    Update streamlit_app.py
    added Avocado Toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberrry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-
+streamlit.text('Avocado Toast')
 
 
 

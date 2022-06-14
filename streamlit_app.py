c7a4a6664e5ddcebaecf2f3e87b9f4cb6c3b62ce
jesty1313 <107418805+jesty1313@users.noreply.github.com>
2022-06-14 08:59:58 -0400

    Update streamlit_app.py
    Added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
 
+streamlit.header('Build Your Own Fruit Smoothie')
+
 
 
 

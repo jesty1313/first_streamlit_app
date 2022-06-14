57d7149e9853a888ca75ebb139035e005fa952ac
jesty1313 <107418805+jesty1313@users.noreply.github.com>
2022-06-14 08:56:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,15 @@ import streamlit
 
 streamlit.title('My Parents New Heathly Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('  :bowtie: Omega 3 & Blueberrry Oatmeal')
+streamlit.header('Breakfast Favorites')
+streamlit.text('Omega 3 & Blueberrry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 
 
 
-https://gist.github.com/rxaviers/7360908#file-gistfile1-md
+
   
 
   

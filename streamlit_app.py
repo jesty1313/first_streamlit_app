17b50434120c550e426568d70255006f55b84103
jesty1313 <107418805+jesty1313@users.noreply.github.com>
2022-06-17 09:02:19 -0400

    Update streamlit_app.py
    Added .json extension to response on advice

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.header('Fruitvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
   

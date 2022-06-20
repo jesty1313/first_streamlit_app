f6a5294f55ed836cadd4249033b071997a966b4a
jesty1313 <107418805+jesty1313@users.noreply.github.com>
2022-06-20 14:54:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,11 +44,10 @@ try:
 except UBLError as e:
   streamlit.error()
 #import snowflake.connector
-my_cur.execute("select * from fruit_load_list")
 streamlit.header("The fruit load list contains:")
 #snowflake-related functions
 def get_fruit_load_list():
-      with my_cnx.cursor() as my_cur
+      with my_cnx.cursor() as my_cur:
       my_cur.execute("select * from fruit_load_list")
       return my_cur.fetchall()
 

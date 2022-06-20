cf1bfa3d5568643fcb7d94f89c944899b7c81df0
jesty1313 <107418805+jesty1313@users.noreply.github.com>
2022-06-20 13:53:02 -0400

    Update streamlit_app.py
    testing insert into snowflake table

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,10 @@ streamlit.dataframe(my_data_rows)
 
 
 fruit_choice = streamlit.text_input('What fruit would you like to add?', 'Apple')
-streamlit.write('The user entered', fruit_choice)
+streamlit.write('Thanks for adding', fruit_choice)
+
+# This will not work correctly, but just go with it for now
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
   
 
